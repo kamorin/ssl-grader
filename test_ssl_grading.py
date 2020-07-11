e896d64370def4f0b434f1c0ba487a1c498f86b5
Kevin Amorin <kevin@bitsighttech.com>
2020-07-11 18:02:15 -0400

    shodan initial test

--- test_ssl_grading.py
+++ test_ssl_grading.py
@@ -1,7 +1,8 @@
 import unittest
 import sys
 import pickle
-import ssl-cert
+from sslcert import *
+import datetime
 
 """
 Unit tests for :ssl-cert.py
@@ -9,16 +10,35 @@ Unit tests for :ssl-cert.py
 domain="wpi.edu"
 
 class TestStringMethods(unittest.TestCase):
+    def test_shodan_load_https_subject(self):
+        mysearch = certSearch("SHODAN", "TESTING134", 1)
+        mysearch.search(domain, True)
+        raw_results = mysearch.get_raw_results()[0]
+        cert = {
+            'altnames': ['*.echo360.wpi.edu'],
+            'cipher': {'bits': 128,
+                        'name': 'ECDHE-RSA-AES128-GCM-SHA256',
+                        'version': 'TLSv1/SSLv3'},
+            'dhparams': {'bits': 4096,
+                        'generator': 2,
+                        'prime': None,
+                        'public_key': None},
+            'expired': False,
+            'expires': '20220507235959Z',
+            'hostname': ['50p2410-echo.echo360.wpi.edu'],
+            'ip': '130.215.192.40',
+            'isp': 'Worcester Polytechnic Institute',
+            'issued': datetime.datetime(2020, 5, 7, 0, 0),
+            'pubkey': {'bits': 2048, 'type': 'rsa'},
+            'server_cert': None,
+            'sig_alg': 'sha256WithRSAEncryption',
+            'subject': '*.echo360.wpi.edu',
+            'trust_chain': None,
+            'version': ['TLSv1', '-SSLv2', '-SSLv3', 'TLSv1.1', 'TLSv1.2', '-TLSv1.3']
+        }
+        self.assertEquals(raw_results["ssl"]["cert"]["pubkey"]['bits'],cert['pubkey']['bits'])
+            
     def test_censys_load_https_subject(self):
-        # cert_search_results = []
-        # print("locading cache")
-        # try:
-        #     with open(f"{domain}-censys.pkl", "rb") as f:
-        #         cert_search_results = pickle.load(f)
-        # except IOError as e:
-        #     print(f"file not found {e}")
-        #     sys.exit(1)
-
         cert= {
             'altnames': ['w.wpi.edu',
                     'www-backend1.wpi.edu',
@@ -39,15 +59,10 @@ class TestStringMethods(unittest.TestCase):
             'validation_reason': True,
             'version': 'TLSv1.2'
         }
-        mysearch = certSearch("CENSYS", "TESTING134", "1")
-        mysearch.search(domain, use_cache=True)
-        
-        for certinfo in mysearch.get_results():
-            cert = graderCert(**certinfo)
-            cert.grade_cert()
-            certs.append(cert)
-            self.assertEquals(,3)
+        #mysearch = certSearch("CENSYS", "TESTING134", "1")
+        #mysearch.search(domain, use_cache=True)
 
 
 
-
+if __name__ == '__main__':
+    unittest.main()
