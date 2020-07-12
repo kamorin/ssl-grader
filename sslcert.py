dd10d59ceb7e176de76fa0c088a9c1cf398d1835
Kevin Amorin <kevin@bitsighttech.com>
2020-07-12 00:21:30 -0400

    refactor certsearch obj

--- sslcert.py
+++ sslcert.py
@@ -19,6 +19,8 @@ import logging
 import argparse
 from prettytable import PrettyTable
 import csv
+import censys.certificates
+import censys.ipv4
 
 ROOT_STORE = None
 
@@ -160,25 +162,51 @@ class graderCert(object):
 class certSearch(object):
     """ facade search obj
     """
-
-    def __init__(self, search_engine, api_key, result_limit):
+    
+    def __init__(self, search_engine, result_limit, api_id, api_secret=None):
         if search_engine == "SHODAN":
-            self.searchAPI = shodanSearch(api_key, result_limit)
+            self.searchAPI = shodanSearch(result_limit, api_id)
         else:
-            self.searchAPI = censysSearch(api_key, result_limit)
+            self.searchAPI = censysSearch(result_limit, api_id, api_secret)
 
-    def load(self, result):
-        self.searchAPI.load(result)
+    def load_raw_results(self):
+        '''  load search results from self.raw_results into common format dict self.results '''
+        certs = []
+        for result in self.searchAPI.raw_results:
+            # load results and convert it to a dict we can grade
+            certs.append(self.searchAPI.load(result))
+        self.searchAPI.results = certs
 
-    def search(self, domain, load_cache):
-        self.searchAPI.search(domain, load_cache)
+    def search(self, domain, use_cache):
+        ''' load raw cache or  '''
+        self.domain=domain
+        self.raw_results=[]
+        #load cache of raw search results
+        if use_cache:
+            self.load_cache(domain)
+        else:
+            self.searchAPI.search(domain)
+            self.save_cache(domain)
+        self.load_raw_results()
 
     def get_results(self):
         return self.searchAPI.results
 
     def get_raw_results(self):
         return self.searchAPI.raw_results
+    
+    def load_cache(self,filename=None):
+        ''' load raw search results into raw_results from pickled file'''
+        try:
+            log(f"-Reading cached data from {filename}.pkl\n", "INFO")
+            with open(f"{filename}.pkl", "rb") as f:
+                self.searchAPI.raw_results = pickle.load(f)
+        except IOError:
+            log(f"-Cache file not accessible for {filename}", "INFO")
 
+    def save_cache(self,filename=None):
+        pickle.dump(self.searchAPI.raw_results, open(f"{filename}.pkl", "wb"))
+  
 class censysSearch(object):
     """ censys obj """
     RESULT_FIELDS = ['443','11211','143','1433','161','25','3306','3389','465','5432','5672','631','6379','6443','8883','ip','993','995']
@@ -204,7 +232,7 @@ class censysSearch(object):
                     '995.pop3s.tls.tls.certificate.parsed.names',
                     ]
 
-    def __init___(self, api_key=None, result_limit=100):
+    def __init__(self, result_limit, api_id, api_secret):
         # parse SEARCHFIELDs and create a dict pointing to TLS 
         self.search_key={}
         for item in self.SEARCH_FIELDS:
@@ -212,12 +240,14 @@ class censysSearch(object):
             self.search_key[terms[0]]=terms[:-3]
 
         self.result_limit = result_limit
-        if api_key:
-            self.SHODAN_API = api_key
-        elif os.getenv("CENSYS_API", None):
-            self.SHODAN_API = os.environ["CENSYS_API"]
+        if api_id:
+            self.CENSYS_API_ID = api_id
+            self.CENSYS_API_SECRET = api_secret
+        elif os.getenv("CENSYS_API_ID", None) and os.getenv("CENSYS_API_SECRET", None): 
+            self.CENSYS_API_ID = os.environ["CENSYS_API_ID"]
+            self.CENSYS_API_SECRET = os.environ["CENSYS_API_SECRET"]
         else:
-            log("CENSYS_API Key missing.  Pass as argument or set SHODAN_API env var", "ERROR")
+            log("CENSYS_API_ID or CENSYS_API_SECRET Key missing.  Pass as argument or set env vars", "ERROR")
             sys.exit(1)
 
     def load(self,result):
@@ -277,14 +307,39 @@ class censysSearch(object):
 
         return certinfo
 
-    def search(self):
-        pass
+    def search(self, domain):
+        """  call Shodan API and save result list to self.raw_results
+        """
+        query = ' '.join([ '{0}:"{1}"'.format(x, domain) for x in self.SEARCH_FIELDS ])
+        log(f"**Querying Censys with search query {query}\n", "INFO")
+
+        censys_cert= censys.ipv4.CensysIPv4(api_id=censys_id,api_secret=censys_secret)
+        try:
+            api = censys_cert.search(query, self.RESULT_FIELDS, flatten=False)
+            self.raw_results = list(api.search_cursor(query))
+            pickle.dump(list(api), open(f"{domain}-censys.pkl", "wb"))
+        except censys.base.CensysUnauthorizedException:
+            sys.stderr.write('[+] Censys account details wrong. \n')
+            exit(1)
+        except censys.base.CensysRateLimitExceededException:
+            sys.stderr.write('[+] Limit exceeded.')
+            exit(1)
+        except censys.base.CensysException as e:
+            # catch the Censys Base exception, example "only 1000 first results are available"
+            sys.stderr.write('[-] Exception?, ' + repr(e))
+            exit(1) 
+
+    def get_results(self):
+        return self.results
+
+    def get_raw_results(self):
+        return self.raw_results
 
 
 class shodanSearch(object):
     """ shodan search api
     """
-    def __init__(self, api_key=None, result_limit=100):
+    def __init__(self, result_limit, api_key):
         self.result_limit = result_limit
         if api_key:
             self.SHODAN_API = api_key
@@ -300,52 +355,14 @@ class shodanSearch(object):
     def get_raw_results(self):
         return self.raw_results
 
-    def search(self, domain, use_cache=False):
+    def search(self, domain):
         """  call Shodan API and save result list to self.raw_results
         """
-        self.raw_results=[]
-        #load cache of raw search results
-        if use_cache:
-            try:
-                log(f"-LOCAL REPORT GENERATION\n-NOT CALLING SHODAN\n-Reading cached data from {domain}.pkl\n", "INFO")
-                with open(f"{domain}.pkl", "rb") as f:
-                    self.raw_results = pickle.load(f)
-            except IOError:
-                log(f"-Cache file not accessible for {domain}", "INFO")
-
-        if not self.raw_results:
-            api = Shodan(self.SHODAN_API)
-            query = "ssl.cert.subject.cn:" + domain
-            log(f"**Querying Shodan with Search query {query}\n", "INFO")
-            mylist=list(api.search_cursor(query))
-            #self.raw_results=list(api.search_cursor(query))
-            self.raw_results=mylist[0]
-
-        if use_cache:
-            pickle.dump(self.raw_results, open(f"{domain}.pkl", "wb"))
-    
-        self.load_raw_results()
+        api = Shodan(self.SHODAN_API)
+        query = "ssl.cert.subject.cn:" + domain
+        log(f"**Querying Shodan with Search query {query}\n", "INFO")
+        self.raw_results=list(api.search_cursor(query))
         
-            
-    def load_raw_results(self):
-        '''  load shodan results from self.raw_results into common format dict self.results
-        '''
-        limit = self.result_limit
-        counter = 0
-        certs = []
-        for result in self.raw_results:
-            # html large result, del now and save space
-            result.pop("html", None)
-
-            # load shodan results and convert it to a dict we can grade
-            certs.append(self.load(result))
-            counter += 1
-            if counter >= limit:
-                break
-
-        self.results = certs
-
-
     def load(self, result):
         """ take shodan result dict and convert it to a dict for use in grading
         """
@@ -379,7 +396,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="ssl-cert.py", description="ssl-cert grader")
     parser.add_argument("domain", help="subdomain to search for certificates")
     parser.add_argument("-s", required=False, dest="api_key_shodan", help="Shodan API key")
-    parser.add_argument("-c", required=False, dest="api_key_censys", help="Censys API key")
+    parser.add_argument("--censys_id", required=False, dest="api_key_censys_id", help="Censys API ID")
+    parser.add_argument("--censys_secret", required=False, dest="api_key_censys_secret", help="Censys API Secret")
     parser.add_argument("-o", required=False, dest="csv_output", action="store_true", default=False, help="output report to a CSV file")
     parser.add_argument("-l", required=False, dest="result_limit", type=int, default=100, action="store", help="limit result set to save on API credits")
     parser.add_argument("-u", required=False, dest="use_cache", action="store_true", default=False, help="used cache to generate report")
@@ -397,8 +415,8 @@ if __name__ == "__main__":
     ROOT_STORE = load_root_ca_list()
 
     certs = []
-    mysearch = certSearch("SHODAN", args.api_key_shodan, args.result_limit)
-    #mysearch = certSearch("CENSYS", args.api_key_censys, args.result_limit)
+    mysearch = certSearch("SHODAN", args.result_limit, args.api_key_shodan, )
+    #mysearch = certSearch("CENSYS", args.result_limit, args.api_key_censys_id, args.api_key_censys_secret)
     mysearch.search(domain, use_cache)
 
     for certinfo in mysearch.get_results():
